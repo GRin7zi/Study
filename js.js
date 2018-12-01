73a5c8a97c73af74cbf859e301deca0d7e10666e
GR <grin7zi@yandex.ru>
2018-12-01 15:03:47 +0300

    this is fFirst commit

---
+++ js.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+console.log(4/0);
+console.log("string" * 9);
+var som;
+console.log(som);
+
+var person = {
+    name: "GR",
+    age: 25,
+    isMarried: true
+};
+
+console.log(person.name+" "+person.age);
+
+
+var incr = 30, decr = 10;
+
+console.log(++incr);
+console.log(++decr);
+console.log(6%4);
+console.log("2" == 2);
+console.log("2" === 2);
